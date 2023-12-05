fe3829046cdd66effa11bb626b5dec16b3142f7b
DanielNeres <danielnerres@gmail.com>
2023-12-05 14:28:56 -0300

    Sequencia de Fabiano ao Contrario V.3.0

--- Sequencia de Fabiano ao Contrario.c
+++ Sequencia de Fabiano ao Contrario.c
@@ -3,8 +3,9 @@
 
 int main(){
 	int numeroimprecao, i; 
-
+// incia um for que percorre todas a sequencia de fibonacci, começando de i até i=0
 	for (i = 10; i >= 0; i--) {
+	// para calcular a sequencia de Fibonacci uso a formula: F(i) = (pow((1 + sqrt(5)), i) - pow((1 - sqrt(5)), i)) / (pow(2, i) * sqrt(5))
 		numeroimprecao = (pow((1 + sqrt(5)), i) - (pow((1 - sqrt(5)), i))) /(pow(2, i) * sqrt(5));
         printf("%d\n", numeroimprecao);
     }
