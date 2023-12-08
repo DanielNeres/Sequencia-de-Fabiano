51cd54a17be9769c01c46a1970fa12051c410c4b
DanielNeres <danielnerres@gmail.com>
2023-12-08 18:53:58 -0300

    Sequencia de Fabiano Ultra Otimizada V.2

--- Sequencia de Fabiano Ultra Otimizada.c
+++ Sequencia de Fabiano Ultra Otimizada.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 
 int main(){
-	int numeroimprecao = 0, numeroanterior = 1; 
+	unsigned long int numeroimprecao = 0, numeroanterior = 1; 
 	// inicia um for que define o tamanho da sequência atravez da variavel i
-	for(;numeroimprecao < 34;){
+	for(;numeroimprecao <= 34;){
 	// importante imprimir o numero antes que o valor seja modificado	
-		printf("\n%d", numeroimprecao); 
+		printf("\n%li", numeroimprecao); 
 	// soma o numero de impreção a outra variavel, que no incio serve para inciar a sequencia, mas após o primeiro laço ele reistra o ultimp balor usado na impreção	
 		numeroimprecao += numeroanterior; 
 	// atualiza o numero anterior que foi usado na impreção, por meio da diferença entre o numero impreço e o numero anterior ao anterior deste, por exemplo 8, e feita a diferença entre 8 e dois numeros anteriores a ele, assim ficaria 8 - 3 = 5 que é o numero anterior a 8
