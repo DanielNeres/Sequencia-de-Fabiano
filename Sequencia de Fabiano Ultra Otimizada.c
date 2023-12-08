59affe821ee61f614aeacdaad1aacba09c425f1a
DanielNeres <danielnerres@gmail.com>
2023-12-08 18:57:03 -0300

    Sequencia de Fabiano Ultra Otimizada V.3

--- Sequencia de Fabiano Ultra Otimizada.c
+++ Sequencia de Fabiano Ultra Otimizada.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 
 int main(){
+// as declarações "unsigned long int" serve para tornar a variavel a maior possivel, pois o "unsigned" retira o sinal liberando um bit, e o "long" estende a variavel int
 	unsigned long int numeroimprecao = 0, numeroanterior = 1; 
-	// inicia um for que define o tamanho da sequência atravez da variavel i
 	for(;numeroimprecao <= 34;){
 	// importante imprimir o numero antes que o valor seja modificado	
 		printf("\n%li", numeroimprecao); 
-	// soma o numero de impreção a outra variavel, que no incio serve para inciar a sequencia, mas após o primeiro laço ele reistra o ultimp balor usado na impreção	
+	// soma o numero de impreção a outra variavel, que no incio serve para inciar a sequencia, mas após o primeiro laço ele reistra o ultimo balor usado na impreção	
 		numeroimprecao += numeroanterior; 
 	// atualiza o numero anterior que foi usado na impreção, por meio da diferença entre o numero impreço e o numero anterior ao anterior deste, por exemplo 8, e feita a diferença entre 8 e dois numeros anteriores a ele, assim ficaria 8 - 3 = 5 que é o numero anterior a 8
 	// vale salientar que essa variavel só começa a registrar o numero anterior a impreção ao final do primeiro laço, e que essa logica só começa a fazer sentido após o primeiro laço
