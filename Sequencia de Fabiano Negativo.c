e291eac8693e01cb88c54e01bf08f1c6e75c29a7
Daniel Neres <146374053+DanielNeres@users.noreply.github.com>
2023-12-13 09:04:00 -0300

    Update Sequencia de Fabiano Negativo V.3.2

--- Sequencia de Fabiano Negativo.c
+++ Sequencia de Fabiano Negativo.c
@@ -6,9 +6,9 @@ int main(){
 	for(i = 0; i < 10; i++){
 	// importante imprimir o numero antes que o valor seja modificado	
 		printf("\n%d", numeroimprecao); 
-	// soma o numero de impreção a outra variavel, que no incio serve para inciar a sequencia, mas após o primeiro laço ele reistra o ultimp balor usado na impreção	
+	// soma o numero de impressão a outra variavel, que no incio serve para inciar a sequencia, mas após o primeiro laço ele reistra o ultimo valor usado na impreção	
 		numeroimprecao += numeroanterior; 
-	// atualiza o numero anterior que foi usado na impreção, por meio da diferença entre o numero impreço e o numero anterior ao anterior deste, por exemplo 8, e feita a diferença entre 8 e dois numeros anteriores a ele, assim ficaria (-8) - (-3) = -5 que é o numero anterior a 8
+	// atualiza o numero anterior que foi usado na impressão, por meio da diferença entre o numero impreço e o numero anterior ao anterior deste, por exemplo -8, e feita a diferença entre -8 e dois numeros anteriores a ele, assim ficaria (-8) - (-3) = -5 que é o numero anterior a -8
 	// vale salientar que essa variavel só começa a registrar o numero anterior a impreção ao final do primeiro laço, e que essa logica só começa a fazer sentido após o primeiro laço
 		numeroanterior = numeroimprecao - numeroanterior; 
 	}
